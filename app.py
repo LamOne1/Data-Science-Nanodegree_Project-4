e0c6f6b3d94f9cb3efb0db2c644c66c8378634b1
LamyaMK <97054802+LamyaMK@users.noreply.github.com>
2022-05-18 21:08:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask import request, url_for, render_template, redirect
 from extract_bottleneck_features import *
 
 from keras.preprocessing import image                  
-from keras.applications.resnet50 import preprocess_input
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 
 from keras.models import Sequential#, load_model
 from keras.layers import GlobalAveragePooling2D, Dense
 import numpy as np
 import os
-
+import cv2
 from keras import backend as K
 
 dogs_names_file = open('dogs_name.txt','r')
@@ -31,12 +31,29 @@ def Resnet50_predict_breed(img_path):
 
     # obtain predicted vector
     model = load_model()
+    
 
     predicted_vector = model.predict(bottleneck_feature)
+    K.clear_session()
     # return dog breed that is predicted by the model
     #return dog_names[np.argmax(predicted_vector)]
     return dogs_names[np.argmax(predicted_vector)]
 
+def dog_detector(img_path):
+    ResNet50_model_imagenet  = ResNet50(weights='imagenet')
+    
+    img = preprocess_input(path_to_tensor(img_path))
+    prediction = np.argmax(ResNet50_model_imagenet.predict(img))
+    K.clear_session()
+    return ((prediction <= 268) & (prediction >= 151)) 
+
+def face_detector(img_path):
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    K.clear_session()
+    return len(faces) > 0
 
 def path_to_tensor(img_path):
     # loads RGB image as PIL.Image.Image type
@@ -70,14 +87,23 @@ def predict():
             image_ = flask.request.files["image"]
             image = image_.save(path_img)
             
-            predictions = Resnet50_predict_breed(path_img)
             
+            if dog_detector(path_img):
+                prediction = Resnet50_predict_breed(path_img)
+                data["prediction"] = 'It\'s an image of a dog! its breed is ' + prediction
+        
+            elif face_detector(path_img):
+                prediction= Resnet50_predict_breed(path_img)
+                data["prediction"] = 'It\'s an image of a human! it resembles ' + prediction
+                
+            else:        
+                data["prediction"] = 'It\'s not clear whether it\'s a human or a dog'
             
-            data["prediction"] = predictions
-            
-            return render_template('master.html', data=data, name=path_img)
 
-    return render_template('master.html', data = data, name=path_img)
+            
+            return render_template('master.html', data=data)
+
+    return render_template('master.html', data = data)
     
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=3000, debug=True)
